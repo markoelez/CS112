c3cee4bc92c1647b7a9cdc8c22f2f5241977950e
Marko <markoelez7@gmail.com>
2019-10-01 19:09:13 -0400

    finished big int

--- Big Integer/src/bigint/BigInteger.java
+++ Big Integer/src/bigint/BigInteger.java
@@ -4,7 +4,7 @@ package bigint;
  * This class encapsulates a BigInteger, i.e. a positive or negative integer with 
  * any number of digits, which overcomes the computer storage length limitation of 
  * an integer.
- * 
+ *
  */
 public class BigInteger {
 
@@ -12,23 +12,23 @@ public class BigInteger {
 	 * True if this is a negative integer
 	 */
 	boolean negative;
-	
+
 	/**
 	 * Number of digits in this integer
 	 */
 	int numDigits;
-	
+
 	/**
 	 * Reference to the first node of this integer's linked list representation
 	 * NOTE: The linked list stores the Least Significant Digit in the FIRST node.
 	 * For instance, the integer 235 would be stored as:
 	 *    5 --> 3  --> 2
-	 *    
+	 *
 	 * Insignificant digits are not stored. So the integer 00235 will be stored as:
 	 *    5 --> 3 --> 2  (No zeros after the last 2)        
 	 */
 	DigitNode front;
-	
+
 	/**
 	 * Initializes this integer to a positive number with zero digits, in other
 	 * words this is the 0 (zero) valued integer.
@@ -49,85 +49,13 @@ public class BigInteger {
 	}
 
 	private static int getNumDigits(DigitNode node) {
-		int i = 0;
-		DigitNode ref = node;
-		if (ref == null) {
-			return 0;
+		DigitNode temp = node;
+		int count = 0;
+		while (temp != null) {
+			count++;
+			temp = temp.next;
 		}
-		while (ref.next != null) {
-			ref = ref.next;
-			i++;
-		}
-		return i;
-	}
-	
-	/**
-	 * Parses an input integer string into a corresponding BigInteger instance.
-	 * A correctly formatted integer would have an optional sign as the first 
-	 * character (no sign means positive), and at least one digit character
-	 * (including zero). 
-	 * Examples of correct format, with corresponding values
-	 *      Format     Value
-	 *       +0            0
-	 *       -0            0
-	 *       +123        123
-	 *       1023       1023
-	 *       0012         12  
-	 *       0             0
-	 *       -123       -123
-	 *       -001         -1
-	 *       +000          0
-	 *       
-	 * Leading and trailing spaces are ignored. So "  +123  " will still parse 
-	 * correctly, as +123, after ignoring leading and trailing spaces in the input
-	 * string.
-	 * 
-	 * Spaces between digits are not ignored. So "12  345" will not parse as
-	 * an integer - the input is incorrectly formatted.
-	 * 
-	 * An integer with value 0 will correspond to a null (empty) list - see the BigInteger
-	 * constructor
-	 * 
-	 * @param integer Integer string that is to be parsed
-	 * @return BigInteger instance that stores the input integer.
-	 * @throws IllegalArgumentException If input is incorrectly formatted
-	 */
-	public static BigInteger parse(String integer) 
-	throws IllegalArgumentException {
-		String str = integer.trim();
-		BigInteger bigInt = new BigInteger();
-
-		// get rid of zeros in front and check format
-		if (str.charAt(0) == '-' || str.charAt(0) == '+') {
-			char sign = str.charAt(0);
-			if (sign == '-') {
-				bigInt.negative = true;
-			} else if (sign == '+'){
-				bigInt.negative = false;
-			}
-			str = removeLeadingZeros(str.substring(1));
-		} else if (Character.isDigit(str.charAt(0))){
-			str = removeLeadingZeros(str);
-		} else {
-			throw new IllegalArgumentException("Incorrect format");
-		}
-
-		bigInt.numDigits = str.length();
-
-		// create linked list root
-		int start_idx = str.length()-1;
-		bigInt.front = new DigitNode(Character.getNumericValue(str.charAt(start_idx)), null);
-		// build the rest
-		DigitNode ptr = bigInt.front;
-		for (int i = start_idx-1; i>-1; i--) {
-			if (Character.isDigit(str.charAt(i))) {
-				ptr.next = new DigitNode(Character.getNumericValue(str.charAt(i)), null);
-			} else {
-				throw new IllegalArgumentException("Incorrect format");
-			}
-			ptr = ptr.next;
-		}
-		return bigInt;
+		return count;
 	}
 
 	private static DigitNode padNode(DigitNode snode, int diff) {
@@ -176,15 +104,82 @@ public class BigInteger {
 		return node;
 	}
 
-
-	
 	/**
-	 * Adds the first and second big integers, and returns the result in a NEW BigInteger object. 
+	 * Parses an input integer string into a corresponding BigInteger instance.
+	 * A correctly formatted integer would have an optional sign as the first
+	 * character (no sign means positive), and at least one digit character
+	 * (including zero).
+	 * Examples of correct format, with corresponding values
+	 *      Format     Value
+	 *       +0            0
+	 *       -0            0
+	 *       +123        123
+	 *       1023       1023
+	 *       0012         12
+	 *       0             0
+	 *       -123       -123
+	 *       -001         -1
+	 *       +000          0
+	 *
+	 * Leading and trailing spaces are ignored. So "  +123  " will still parse
+	 * correctly, as +123, after ignoring leading and trailing spaces in the input
+	 * string.
+	 *
+	 * Spaces between digits are not ignored. So "12  345" will not parse as
+	 * an integer - the input is incorrectly formatted.
+	 *
+	 * An integer with value 0 will correspond to a null (empty) list - see the BigInteger
+	 * constructor
+	 *
+	 * @param integer Integer string that is to be parsed
+	 * @return BigInteger instance that stores the input integer.
+	 * @throws IllegalArgumentException If input is incorrectly formatted
+	 */
+	public static BigInteger parse(String integer)
+			throws IllegalArgumentException {
+		String str = integer.trim();
+		BigInteger bigInt = new BigInteger();
+
+		// get rid of zeros in front and check format
+		if (str.charAt(0) == '-' || str.charAt(0) == '+') {
+			char sign = str.charAt(0);
+			if (sign == '-') {
+				bigInt.negative = true;
+			} else if (sign == '+'){
+				bigInt.negative = false;
+			}
+			str = removeLeadingZeros(str.substring(1));
+		} else if (Character.isDigit(str.charAt(0))){
+			str = removeLeadingZeros(str);
+		} else {
+			throw new IllegalArgumentException("Incorrect format");
+		}
+
+		bigInt.numDigits = str.length();
+
+		// create linked list root
+		int start_idx = str.length()-1;
+		bigInt.front = new DigitNode(Character.getNumericValue(str.charAt(start_idx)), null);
+		// build the rest
+		DigitNode ptr = bigInt.front;
+		for (int i = start_idx-1; i>-1; i--) {
+			if (Character.isDigit(str.charAt(i))) {
+				ptr.next = new DigitNode(Character.getNumericValue(str.charAt(i)), null);
+			} else {
+				throw new IllegalArgumentException("Incorrect format");
+			}
+			ptr = ptr.next;
+		}
+		return bigInt;
+	}
+
+	/**
+	 * Adds the first and second big integers, and returns the result in a NEW BigInteger object.
 	 * DOES NOT MODIFY the input big integers.
-	 * 
+	 *
 	 * NOTE that either or both of the input big integers could be negative.
 	 * (Which means this method can effectively subtract as well.)
-	 * 
+	 *
 	 * @param first First big integer
 	 * @param second Second big integer
 	 * @return Result big integer
@@ -241,7 +236,7 @@ public class BigInteger {
 			if (negative) {
 				res.negative = true;
 			}
-			// ADD NUM DIGITS
+			res.numDigits = getNumDigits(res.front);
 			return res;
 		} else {
 			// SUBTRACTION
@@ -333,23 +328,21 @@ public class BigInteger {
 			}
 			ans.negative = negative;
 			// debug
-			traverseNodes(ans.front);
+//			traverseNodes(ans.front);
 
 			ans.front = removeTrailingZeros(ans.front);
 			ans.numDigits = getNumDigits(ans.front);
-			traverseNodes(ans.front);
+//			traverseNodes(ans.front);
 			return ans;
 		}
 	}
 
-
-	
 	/**
 	 * Returns the BigInteger obtained by multiplying the first big integer
 	 * with the second big integer
-	 * 
+	 *
 	 * This method DOES NOT MODIFY either of the input big integers
-	 * 
+	 *
 	 * @param first First big integer
 	 * @param second Second big integer
 	 * @return A new BigInteger which is the product of the first and second big integers
@@ -415,10 +408,11 @@ public class BigInteger {
 
 		res.front = removeTrailingZeros(res.front);
 		res.numDigits = getNumDigits(res.front);
+//		traverseNodes(res.front);
 		return res;
 	}
-	
-	
+
+
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
@@ -428,14 +422,12 @@ public class BigInteger {
 		}
 		String retval = front.digit + "";
 		for (DigitNode curr = front.next; curr != null; curr = curr.next) {
-				retval = curr.digit + retval;
+			retval = curr.digit + retval;
 		}
-		
+
 		if (negative) {
 			retval = '-' + retval;
 		}
 		return retval;
 	}
-
-
 }
--- Big Integer/src/bigint/Driver.java
+++ Big Integer/src/bigint/Driver.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Driver {
 
     public static void main(String args[]) throws Exception {
+//        test();
         multTest();
         addTest();
         System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
@@ -14,11 +15,51 @@ public class Driver {
         System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
     }
 
+    private static void traverseNodes(DigitNode node) {
+        // utility function to traverse and print nodes of a linked list
+        System.out.println("----------------------------------");
+        DigitNode n = node;
+        while (n != null) {
+            System.out.print(n.digit + " ");
+            n = n.next;
+        }
+        System.out.println("\n----------------------------------");
+    }
+
+    private static void test() throws Exception {
+        String t1 = "95465";
+        String t2 = "20802";
+
+        try {
+            BigInteger one = BigInteger.parse(t1);
+            BigInteger two = BigInteger.parse(t2);
+
+            System.out.println(t1);
+            System.out.println(t2);
+
+            BigInteger ans = BigInteger.multiply(one, two);
+
+            System.out.println("----------------------------------------------------------------------------");
+            traverseNodes(ans.front);
+            System.out.println("----------------------------------------------------------------------------");
+            System.out.println(ans.numDigits);
+            System.out.println("----------------------------------------------------------------------------");
+            System.out.println(ans.negative);
+            System.out.println("----------------------------------------------------------------------------");
+
+        } catch (Exception e) {
+            System.out.println(t1);
+            System.out.println(t2);
+            throw new Exception();
+        }
+
+    }
+
     private static void multTest() throws Exception{
         int i = 0;
         while (i < 100000) {
-            String t1 = Integer.toString(ThreadLocalRandom.current().nextInt(-10000, 10000));
-            String t2 = Integer.toString(ThreadLocalRandom.current().nextInt(-10000, 10000));
+            String t1 = Integer.toString(ThreadLocalRandom.current().nextInt(0, 10000));
+            String t2 = Integer.toString(ThreadLocalRandom.current().nextInt(0, 10000));
 
             try {
                 BigInteger one = BigInteger.parse(t1);
@@ -27,12 +68,21 @@ public class Driver {
                 System.out.println(t1);
                 System.out.println(t2);
 
-                int test = Integer.parseInt(BigInteger.multiply(one, two).toString());
+                BigInteger ans = BigInteger.multiply(one, two);
+
+                int test = Integer.parseInt(ans.toString());
                 int actual = Integer.parseInt(t1) * Integer.parseInt(t2);
+
+                int len1 = ans.numDigits;
+                int len2 = String.valueOf(actual).length();
 
                 if (test != actual) {
                     System.out.println("Test: " + test + " :::: " + "Actual: " + actual);
-                    throw new Exception("Failed");
+                    throw new Exception("Failed answer!");
+                } else if (len1 != len2){
+                    System.out.println("Test: " + test + " :::: " + "Actual: " + actual);
+                    System.out.println("Test Digits: " + len1 + " :::: " + "Actual Digits: " + len2);
+                    throw new Exception("Failed Num Digits");
                 } else {
                     System.out.println("----------------------------------------------------------------------------");
                     System.out.println("Passed!");
@@ -54,8 +104,8 @@ public class Driver {
     private static void addTest() throws Exception{
         int i = 0;
         while (i < 100000) {
-            String t1 = Integer.toString(ThreadLocalRandom.current().nextInt(-100000, 100000));
-            String t2 = Integer.toString(ThreadLocalRandom.current().nextInt(-100000, 100000));
+            String t1 = Integer.toString(ThreadLocalRandom.current().nextInt(0, 100000));
+            String t2 = Integer.toString(ThreadLocalRandom.current().nextInt(0, 100000));
 
             try {
                 BigInteger one = BigInteger.parse(t1);
@@ -64,12 +114,20 @@ public class Driver {
                 System.out.println(t1);
                 System.out.println(t2);
 
-                int test = Integer.parseInt(BigInteger.add(one, two).toString());
+                BigInteger ans = BigInteger.add(one, two);
+
+                int test = Integer.parseInt(ans.toString());
                 int actual = Integer.parseInt(t1) + Integer.parseInt(t2);
+
+                int len1 = ans.numDigits;
+                int len2 = String.valueOf(actual).length();
 
                 if (test != actual) {
                     System.out.println("Test: " + test + " :::: " + "Actual: " + actual);
-                    throw new Exception("Failed");
+                    throw new Exception("Failed answer!");
+                } else if (len1 != len2){
+                    System.out.println("Test Digits: " + len1 + " :::: " + "Actual Digits: " + len2);
+                    throw new Exception("Failed Num Digits");
                 } else {
                     System.out.println("----------------------------------------------------------------------------");
                     System.out.println("Passed!");
